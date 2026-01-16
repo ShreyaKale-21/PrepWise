6396ce67c76a21b923aad72e837eafc82fcfd64c
Shreya Kale <153973673+ShreyaKale-21@users.noreply.github.com>
2026-01-16 13:30:53 +0530

    Add files via upload

---
+++ components/AuthForm.tsx
@@ -0,0 +1,128 @@
+"use client"
+import React from 'react'
+import Link from 'next/link'
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import Image from 'next/image'
+import { toast } from 'sonner'
+import FormField from './FormField'
+import { useRouter } from 'next/navigation'
+import { auth } from "@/firebase/client"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { signIn, signUp } from '@/lib/action/auth.action'
+
+
+
+const authFormSchema = (type: FormType) => {
+	return z.object({
+		name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+		email: z.email(),
+		password: z.string().min(3),
+
+	})
+}
+
+const AuthForm = ({type} : {type: FormType}) => {
+	const router = useRouter()
+	const formSchema = authFormSchema(type);
+
+  // 1. Define your form.
+  	const form = useForm<z.infer<typeof formSchema>>({
+    	resolver: zodResolver(formSchema),
+    	defaultValues: {
+      	name: "",
+		email: "",
+		password: "",
+    	},
+  	})
+
+  // 2. Define a submit handler.
+  	async function onSubmit(values: z.infer<typeof formSchema>) {
+    	try {
+			if(type === 'sign-up'){
+				const {name, email, password } = values;
+
+				const userCredentials = await createUserWithEmailAndPassword(auth , email, password);
+
+				const result = await signUp({
+					uid: userCredentials.user.uid,
+					name: name!,
+					email,
+					password,
+				})
+
+				if(!result?.success) {
+					toast.error(result?.message);
+					return;
+				}
+
+				toast.success("Account created successfully. Please sign in.")
+				router.push('/sign-in')
+			} else {
+				const {email, password } = values;
+
+				const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+
+				const idToken = await userCredentials.user.getIdToken();
+
+				if(!idToken) {
+					toast.error('Sign in failed')
+				}
+				await signIn({
+					email, idToken
+				})
+
+				toast.success("Sign in sucessfully.")
+				router.push('/')
+			}
+		} 
+		catch(error) {
+			console.log(error);
+			toast.error(`There was an error: ${error}`)
+		}
+  	}
+
+	const isSignIn = type === 'sign-in';
+  	return (
+    	<div className='border-gradient p-0.5 rounded-2xl w-fit lg-min:-w-[566px]'>
+      		<div className='flex flex-col gap-6 py-14 px-10 dark-gradient rounded-2xl min-h-full'>
+        		<div className='flex flex-row gap-2 justify-center'>
+          			<Image src="/logo.svg" alt="logo" height={32} width={38}/>
+
+          			<h2 className='text-primary-100'>PrepWise</h2>
+        		</div>
+
+          		<h3>Practice job interview with AI</h3>
+      
+    			<Form {...form}>
+			        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full mt-4 form">
+			          	{!isSignIn && (
+							<FormField control={form.control} name='name' label='Name' placeholder='Your Name'
+							type = 'text'/>
+						)}
+						<FormField control={form.control} name='email' label='Email' placeholder='Your Email address'
+						type='email'/>
+						
+						<FormField control={form.control} name='password' label='Password' placeholder='Your Password'
+						type='password'/>
+
+			          	<Button className="" type="submit">{isSignIn ? 'Sign in' : 'Create an Account'}</Button>
+			        </form>
+			    </Form>
+
+				<p className='text-center'>
+					{isSignIn ? 'No account yet?' : 'Have an account already?'}
+					<Link className='font-bold text-user-primary ml-1' href={!isSignIn ? '/sign-in' : '/sign-up'} >
+					{!isSignIn ? 'Sign in' : 'Sign up'}
+					</Link>
+				</p>
+    		</div>
+    	</div>
+  	)
+}
+
+export default AuthForm
